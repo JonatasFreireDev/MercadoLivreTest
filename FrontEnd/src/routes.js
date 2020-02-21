521a339b172a1fb7962e85663fa398e6f1ca1eb8
Jonatas Freire <jonatasf1997@hotmail.com>
2020-02-21 03:28:43 -0300

    Routes and Api

---
+++ FrontEnd/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Search from '~/pages/Search';
+import ResultsSearch from '~/pages/ResultsSearch';
+import ProductDetail from '~/pages/ProductDetail';
+
+export default function Routes() {
+   return (
+      <Switch>
+         <Route path="/" exact component={Search} />
+         <Route path="/items?search=" component={ResultsSearch} />
+         <Route path="/items/:id" component={ProductDetail} />
+      </Switch>
+   );
+}
